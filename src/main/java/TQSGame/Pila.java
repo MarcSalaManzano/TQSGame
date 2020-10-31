425e1aee5163e82ed375620d25a58f8a5a163887
AdrianDelgadoL <adrian.delgadol@e-campus.uab.cat>
2020-10-31 17:26:00 +0100

    Test addCard de Pila pasado.

--- src/main/java/TQSGame/Pila.java
+++ src/main/java/TQSGame/Pila.java
@@ -3,15 +3,28 @@ package TQSGame;
 import java.util.Stack;
 
 public class Pila {
-	private Stack<Carta> cartas = null;
+	private Stack<Carta> cartas = new Stack<>();
 	private String palo;
 	
 	public Pila(String palo) { this.palo = palo; }
 	
 	public void addCard(Carta carta) {
-		cartas.add(carta);
+		if(cartaValida(carta) && !pilaLlena())
+			cartas.push(carta);
 	}
 	
-	public Carta peekCard() { return cartas.peek(); }
+	public Carta peekCard() { 
+		if(!cartas.empty())
+			return cartas.peek(); 
+		return null;
+		}
 	
+	public boolean cartaValida(Carta carta) {
+		if((peekCard() == null) && (carta.getNum() == 1) && (carta.getPalo() == this.palo))
+			return true;
+		
+		return ((carta.getPalo() == this.palo) && carta.getNum() == peekCard().getNum() + 1);
+	}
+	
+	public boolean pilaLlena() { return cartas.size() == 12; }
 }
--- src/test/java/TQSGame/PilaTest.java
+++ src/test/java/TQSGame/PilaTest.java
@@ -7,28 +7,34 @@ import org.junit.Test;
 public class PilaTest {
 
 	@Test
-	public void testAddCard() {
+	public void testAddCard() { //Test de método addCard, con valores frontera
 		Pila pilaOro = new Pila("Oro");
-		Pila pilaEspada = new Pila("Espada");
-		Pila pilaBasto = new Pila("Basto");
-		Pila pilaCopa = new Pila("Copa");
 		
-		assertEquals(pilaOro.peekCard(), null);
+		assertEquals(pilaOro.peekCard(), null); //valor frontera: 0 cartas en la pila
 		
 		pilaOro.addCard(new Carta("Oro", 1));
-		assertEquals(pilaOro.peekCard(), new Carta("Oro", 1));
+		assertEquals(pilaOro.peekCard().getNum(), 1);
+		assertEquals(pilaOro.peekCard().getPalo(), "Oro");
 		
 		pilaOro.addCard(new Carta("Espada", 2));
-		assertEquals(pilaOro.peekCard(), new Carta("Oro", 1));
+		assertEquals(pilaOro.peekCard().getNum(), 1);
+		assertEquals(pilaOro.peekCard().getPalo(), "Oro");
 		
 		pilaOro.addCard(new Carta("Oro", 3));
-		assertEquals(pilaOro.peekCard(), new Carta("Oro", 1));
+		assertEquals(pilaOro.peekCard().getNum(), 1);
+		assertEquals(pilaOro.peekCard().getPalo(), "Oro");
 		
 		for(int i = 2; i <= 12; i++) {
 			pilaOro.addCard(new Carta("Oro", i));
 		}
+
+		assertEquals(pilaOro.peekCard().getNum(), 12);
+		assertEquals(pilaOro.peekCard().getPalo(), "Oro"); //valor de frontera: pila completa
 		
-		assertEquals(pilaOro.peekCard(), new Carta("Oro", 12));
+		pilaOro.addCard(new Carta("Oro", 13));
+		assertEquals(pilaOro.peekCard().getNum(), 12);
+		assertEquals(pilaOro.peekCard().getPalo(), "Oro"); //valor fuera de límites: no hay más de 12 cartas por palo.
+		
 	}
 
 }
